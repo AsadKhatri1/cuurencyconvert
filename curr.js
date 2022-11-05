b712701e2a7de554f8a04952d5d43bb2dd3de9da
AsadKhatri1 <hafizasad398@gmail.com>
2022-11-05 16:25:58 +0500

    My Currsite

---
+++ curr.js
@@ -0,0 +1,8 @@
+let values=document.getElementById("value")
+let result=document.getElementById("result")
+
+function convert(){
+    result.innerHTML="Your amount is "+values.value/217.86+" USD"
+
+}
+console.log(value)
